74f7fd6deaebd7226ddd739cfeb4281282dd51ca
Madison Evans <m.corbinevans@gmail.com>
2023-03-06 20:01:30 -0700

    projects component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import AboutMe from "./Components/AboutMe/AboutMe";
 import Navbar from "./Components/Navbar/Navbar";
-
+import Projects from "./Components/Projects/Projects";
 function App() {
 	return (
 		<div className="flex flex-col md:flex-row">
 			<Navbar />
 			<AboutMe />
+			<Projects />
 		</div>
 	);
 }
