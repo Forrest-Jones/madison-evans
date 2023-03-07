8cc57cab9f2aa2c4cb4d689c6ea36f9d4f5e460d
Madison Evans <m.corbinevans@gmail.com>
2023-03-06 20:07:19 -0700

    basic carousel component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import AboutMe from "./Components/AboutMe/AboutMe";
 import Navbar from "./Components/Navbar/Navbar";
-import Projects from "./Components/Projects/Projects";
+import Carousel from "./Components/Carousel/Carousel";
 function App() {
 	return (
 		<div className="flex flex-col md:flex-row">
 			<Navbar />
 			<AboutMe />
-			<Projects />
+			<Carousel>
+				<div>asdf</div>
+				<div>aaaaaaa</div>
+				<div>asgggggggggdf</div>
+			</Carousel>
 		</div>
 	);
 }
---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function Carousel(props) {
+	const [current, setCurrent] = useState(0);
+	const { children } = props;
+
+	const handlePrevClick = () => {
+		setCurrent(current === 0 ? children.length - 1 : current - 1);
+	};
+
+	const handleNextClick = () => {
+		setCurrent(current === children.length - 1 ? 0 : current + 1);
+	};
+
+	return (
+		<div className="relative">
+			{children[current]}
+			<div className="absolute top-0 bottom-0 left-0 flex items-center">
+				<button
+					className="text-white bg-gray-500 hover:bg-gray-600 rounded-full px-3 py-2 ml-2 focus:outline-none"
+					onClick={handlePrevClick}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						className="h-5 w-5">
+						<path
+							fillRule="evenodd"
+							d="M11.207 4.293a1 1 0 00-1.414 0L4.336 9.293a1 1 0 000 1.414L9.793 16.7a1 1 0 001.414-1.414L7.414 10l3.793-3.793a1 1 0 000-1.414z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</button>
+			</div>
+			<div className="absolute top-0 bottom-0 right-0 flex items-center">
+				<button
+					className="text-white bg-gray-500 hover:bg-gray-600 rounded-full px-3 py-2 mr-2 focus:outline-none"
+					onClick={handleNextClick}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						className="h-5 w-5">
+						<path
+							fillRule="evenodd"
+							d="M8.793 15.707a1 1 0 001.414 0l5.657-5.657a1 1 0 000-1.414L10.207 4.3a1 1 0 00-1.414 1.414L12.586 10l-3.793 3.793a1 1 0 000 1.414z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default Carousel;
