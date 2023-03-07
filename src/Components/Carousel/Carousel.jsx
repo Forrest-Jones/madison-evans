6894e9845fc9148aee43f9f807c87319fbbe3135
Madison Evans <m.corbinevans@gmail.com>
2023-03-06 22:43:41 -0700

    animatePresence added

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import React from "react";
 
 const Carousel = () => {
@@ -18,16 +18,19 @@ const Carousel = () => {
 			</div>
 
 			<div className="mt-8 flex justify-center">
-				<div className="flex w-24 h-24 bg-gray-700 items-center justify-center">
-					<motion.div
-						key={count}
-						initial={{ x: 100 }}
-						animate={{ x: 0 }}
-						className={`flex w-20 h-20 items-center justify-center ${
-							colors[Math.abs(count % 4)]
-						}`}>
-						{count}
-					</motion.div>
+				<div className="relative overflow-hidden flex w-24 h-24 bg-gray-700 items-center justify-center">
+					<AnimatePresence>
+						<motion.div
+							key={count}
+							initial={{ x: 100 }}
+							animate={{ x: 0 }}
+							exit={{ x: -100 }}
+							className={`absolute flex w-20 h-20 items-center justify-center ${
+								colors[Math.abs(count % 4)]
+							}`}>
+							{count}
+						</motion.div>
+					</AnimatePresence>
 				</div>
 			</div>
 		</div>
