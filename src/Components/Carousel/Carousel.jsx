5223aed9d8c5361895d3de58f662533be49a999c
Madison Evans <m.corbinevans@gmail.com>
2023-03-06 22:40:20 -0700

    framer motion added to component

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import * as Icons from "@heroicons/react/solid";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { motion } from "framer-motion";
 import React from "react";
 
 const Carousel = () => {
@@ -8,16 +9,29 @@ const Carousel = () => {
 	return (
 		<div className="text-white">
 			<div className="flex justify-between">
-				<button onClick={() => setCount(count - 1)}>Prev</button>
-				<button onClick={() => setCount(count + 1)}>Next</button>
+				<button onClick={() => setCount(count - 1)}>
+					<FaAngleLeft className="h-10 w-10" />
+				</button>
+				<button onClick={() => setCount(count + 1)}>
+					<FaAngleRight className="h-10 w-10" />
+				</button>
 			</div>
 
 			<div className="mt-8 flex justify-center">
 				<div className="flex w-24 h-24 bg-gray-700 items-center justify-center">
-					<div className="w-20 h-20 bg-red-500">{count}</div>
+					<motion.div
+						key={count}
+						initial={{ x: 100 }}
+						animate={{ x: 0 }}
+						className={`flex w-20 h-20 items-center justify-center ${
+							colors[Math.abs(count % 4)]
+						}`}>
+						{count}
+					</motion.div>
 				</div>
 			</div>
 		</div>
 	);
 };
+let colors = ["bg-red-500", "bg-blue-500", "bg-green-500", "bg-yellow-500"];
 export default Carousel;
