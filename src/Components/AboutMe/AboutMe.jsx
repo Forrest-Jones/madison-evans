0ae9c3bb2a18dc193959a8604f5df3594f9497ca
Madison Evans <m.corbinevans@gmail.com>
2023-03-06 19:50:09 -0700

    image styling updates

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -2,7 +2,7 @@ import profilePic from "../../assets/images/sample-image.jpeg";
 
 function AboutMe() {
 	return (
-		<div className="flex flex-col items-center justify-center py-12 px-12 bg-gray-100 md:flex-row md:py-24 md:px-4">
+		<div className="flex flex-col items-center justify-center bg-gray-100 md:flex-row md:py-24 md:px-4">
 			{ProfilePic()}
 			{Introduction()}
 		</div>
@@ -11,7 +11,7 @@ function AboutMe() {
 	// introduction
 	function Introduction() {
 		return (
-			<div className="mt-6 md:mt-0 md:w-1/2 md:pl-10">
+			<div className="mt-6 md:mt-0 md:w-1/2 md:pl-10 px-6">
 				<h1 className="text-3xl font-bold mb-2">Hi, I'm Madison</h1>
 				<p className="text-lg text-gray-700 mb-6">
 					I'm a passionate designer with a love for creating beautiful and
@@ -25,12 +25,8 @@ function AboutMe() {
 	// Profile Pic
 	function ProfilePic() {
 		return (
-			<div className="md:w-1/2">
-				<img
-					src={profilePic}
-					alt="Madison Profile"
-					className="rounded-full shadow-md"
-				/>
+			<div className="md:w-1/2 w-full">
+				<img src={profilePic} alt="Madison Profile" className="w-full h-full" />
 			</div>
 		);
 	}
