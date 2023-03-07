62a9b0faa8bbf47b25e86056225ad29974a52143
Madison Evans <m.corbinevans@gmail.com>
2023-03-07 00:03:06 -0700

    working carousel concept

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -3,17 +3,31 @@ import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import { motion, AnimatePresence } from "framer-motion";
 import useMeasure from "react-use-measure";
 import React from "react";
-const Carousel = ({ itemArray }) => {
+const Carousel = ({ imageArray }) => {
 	let [count, setCount] = useState(1);
 	let [ref, { width }] = useMeasure();
 	let [tuple, setTuple] = useState([null, count]); // [prev, current]
-
+	console.log(count);
+	function handleLeftClick() {
+		if (count === 1) {
+			setCount(1);
+		} else {
+			setCount(count - 1);
+		}
+	}
+	function handleRightClick() {
+		if (count === imageArray.length) {
+			setCount(imageArray.length);
+		} else {
+			setCount(count + 1);
+		}
+	}
 	if (tuple[1] !== count) {
 		setTuple([tuple[1], count]);
 	}
 
 	let prev = tuple[0];
-	let direction = count > prev ? 1 : -1;
+	let direction = count > prev ? -1 : 1;
 
 	return (
 		<div className="text-white">
@@ -31,24 +45,22 @@ const Carousel = ({ itemArray }) => {
 							animate="center"
 							exit="exit"
 							custom={{ direction, width }}
-							className={`absolute flex w-full h-full items-center justify-center ${
-								colors[Math.abs(count % 4)]
-							}`}
+							className={`absolute flex w-full h-full items-center justify-center`}
 							transition={{ type: "spring", damping: 30, mass: 3 }}>
-							{count}
+							<img src={imageArray[count - 1]} alt={`${count}`} />
 						</motion.div>
 					</AnimatePresence>
 
 					{/* button group */}
-					<div className="absolute z-10 flex justify-between items-center w-full h-full md:w-4/5">
+					<div className="absolute z-10 flex justify-between items-center w-4/5 h-full md:w-4/5">
 						<button
-							onClick={() => setCount(count - 1)}
+							onClick={() => handleLeftClick()}
 							className="flex items-center justify-center text-white  "
 							style={{ transform: "translateX(-50%)" }}>
 							<FaAngleLeft className="w-20 h-20" />
 						</button>
 						<button
-							onClick={() => setCount(count + 1)}
+							onClick={() => handleRightClick()}
 							className="flex items-center justify-center text-white "
 							style={{ transform: "translateX(50%)" }}>
 							<FaAngleRight className="w-20 h-20" />
