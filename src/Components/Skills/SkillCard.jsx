22b6d971d16cd813a0acbb332b151bf6baba6b64
Madison Evans <m.corbinevans@gmail.com>
2023-03-08 17:57:58 -0700

    styling

--- src/Components/Skills/SkillCard.jsx
+++ src/Components/Skills/SkillCard.jsx
@@ -9,8 +9,8 @@ const SkillCard = (props) => {
 				scale: 1.05,
 				transition: { type: "spring", ease: "easeInOut" },
 			}}
-			className="border border-solid border-black border-1 w-1/3 mx-2 mt-8 py-4 required:h-1/3 md:mx-6 md:my-0 md:h-80 md:w-1/3 rounded-md">
-			<img src={icon} alt="icon" className="mx-auto" />
+			className="border border-solid border-black border-1 w-1/2 mx-2 mt-8 py-8 required:h-1/3 md:mx-6 md:my-0 md:h-80 md:w-1/3 rounded-md">
+			<img src={icon} alt="icon" className="mx-auto w-1/3 md:w-auto" />
 			<h2 className="text-center text-lg font-semibold">{title}</h2>
 			<ul className="mx-6">
 				{content.map((content, id) => {
