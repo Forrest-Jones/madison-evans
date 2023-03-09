5c4193bcf9cce5cf3af2ba61a02e542afcec368b
Madison Evans <m.corbinevans@gmail.com>
2023-03-08 18:12:31 -0700

    modal layout established

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -14,7 +14,16 @@ const Modal = ({ showModal, setShowModal }) => {
 					className="absolute text-white top-0 right-0 mr-10 mt-10">
 					<CgClose size="4em" />
 				</button>
-				<div className="text-white border w-4/5 mx-auto mt-1/2 h-3/5 "></div>
+				<div className="text-white border w-4/5 mx-auto mt-1/2 h-3/5 parent">
+					<h1 className="div1">Title</h1>
+					<p className="div2">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+						perferendis consequuntur incidunt vero voluptates vel, aut labore
+						odio maiores. Voluptas.
+					</p>
+					<div className="div3">video goes here</div>
+					<div className="div4">links go here</div>
+				</div>
 			</div>
 		);
 	} else {
