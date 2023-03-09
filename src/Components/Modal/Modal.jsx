ca9141486453af1050dc858eb8461cceda97e050
Madison Evans <m.corbinevans@gmail.com>
2023-03-08 18:03:14 -0700

    modal styling

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -8,12 +8,13 @@ const Modal = ({ showModal, setShowModal }) => {
 	};
 	if (showModal) {
 		return (
-			<div className="fixed w-full h-full bg-opacity-80  bg-black z-10 ">
+			<div className="fixed w-screen h-screen bg-opacity-80 flex bg-black z-10 items-center">
 				<button
 					onClick={toggleModal}
 					className="absolute text-white top-0 right-0 mr-10 mt-10">
 					<CgClose size="4em" />
 				</button>
+				<div className="text-white border w-4/5 mx-auto mt-1/2 h-3/5 "></div>
 			</div>
 		);
 	} else {
