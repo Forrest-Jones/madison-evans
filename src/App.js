cc70ae67f56ce6d48101eeb2f03019f690f949fa
Madison Evans <m.corbinevans@gmail.com>
2023-03-12 10:24:06 -0700

    props set up for project component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,19 @@ import image1 from "./assets/images/w1.jpeg";
 import image2 from "./assets/images/w2.jpeg";
 import image3 from "./assets/images/w3.jpeg";
 import Modal from "./Components/Modal/Modal";
+import sampleVid from "./assets/palette.mp4";
 import { useState } from "react";
 import ModalContext from "./helpers/ModalContext";
 import ProjectGroupContainer from "./Components/ProjectGroupContainer/ProjectGroupContainer";
 
+const project1Content = {
+	title: "Palette Pal",
+	description:
+		"Palette pal is a really cool app. I'm pretty proud of it, therefore, I'm taking this time to make a dummy description for a modal I'm making. In the real thing, this text will actually be meaningful, but rn, I'm just using it to fill out some space and allow me to see how things will eventually be layed out... so... yeah... Here I am. just filling out space. you know me. the usual. lol. asdf. asdf asdf asdf  fjasld;gk; asd;gjkas llaskjdhg laskdh lkajshdglakjshd lgjkashd lgkjhasldkgjhalsdk ga  alskjdgh laksjdgh lkajshdg jkashd glkajsh dglkjahsd lgjkl  s",
+	video: sampleVid,
+	githubLink: "https://github.com/MadisonEvans94/",
+	siteLink: "https://palettepal.herokuapp.com/",
+};
 function App() {
 	const [showModal, setShowModal] = useState(false);
 	const [modalContent, setModalContent] = useState(null);
@@ -41,16 +50,19 @@ function App() {
 									image={image1}
 									setShowModal={setShowModal}
 									showModal={showModal}
+									projectContent={project1Content}
 								/>
 								<Project
 									image={image2}
 									setShowModal={setShowModal}
 									showModal={showModal}
+									projectContent={project1Content}
 								/>
 								<Project
 									image={image3}
 									setShowModal={setShowModal}
 									showModal={showModal}
+									projectContent={project1Content}
 								/>
 							</ProjectGroupContainer>
 						</ModalContext.Provider>
--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,22 +1,14 @@
 import { motion } from "framer-motion";
 import { useContext } from "react";
 import ModalContext from "../../helpers/ModalContext";
-import sampleVid from "../../assets/palette.mp4";
-function Project({ setShowModal, image, showModal }) {
-	const modalContent = {
-		title: "Palette Pal",
-		description:
-			"Palette pal is a really cool app. I'm pretty proud of it, therefore, I'm taking this time to make a dummy description for a modal I'm making. In the real thing, this text will actually be meaningful, but rn, I'm just using it to fill out some space and allow me to see how things will eventually be layed out... so... yeah... Here I am. just filling out space. you know me. the usual. lol. asdf. asdf asdf asdf  fjasld;gk; asd;gjkas llaskjdhg laskdh lkajshdglakjshd lgjkashd lgkjhasldkgjhalsdk ga  alskjdgh laksjdgh lkajshdg jkashd glkajsh dglkjahsd lgjkl  s",
-		video: sampleVid,
-		githubLink: "https://github.com/MadisonEvans94/",
-		siteLink: "https://palettepal.herokuapp.com/",
-	};
+
+function Project({ setShowModal, showModal, projectContent, image }) {
 	const { setModalContent } = useContext(ModalContext);
 	const toggleModal = () => {
 		setShowModal(!showModal);
 	};
 	const handleProjectClick = () => {
-		setModalContent(modalContent);
+		setModalContent(projectContent);
 		toggleModal();
 	};
 
