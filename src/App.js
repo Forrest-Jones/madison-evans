d173b158164ca371461320236d4f1c49bf055bb5
Madison Evans <m.corbinevans@gmail.com>
2023-03-12 10:58:49 -0700

    accurate descriptions added to project section

--- src/App.js
+++ src/App.js
@@ -18,8 +18,7 @@ import ProjectGroupContainer from "./Components/ProjectGroupContainer/ProjectGro
 // Palette Pal
 const project1Content = {
 	title: "Palette Pal",
-	description:
-		"Palette pal is a really cool app. I'm pretty proud of it, therefore, I'm taking this time to make a dummy description for a modal I'm making. In the real thing, this text will actually be meaningful, but rn, I'm just using it to fill out some space and allow me to see how things will eventually be layed out... so... yeah... Here I am. just filling out space. you know me. the usual. lol. asdf. asdf asdf asdf  fjasld;gk; asd;gjkas llaskjdhg laskdh lkajshdglakjshd lgjkashd lgkjhasldkgjhalsdk ga  alskjdgh laksjdgh lkajshdg jkashd glkajsh dglkjahsd lgjkl  s",
+	description: `"Palette Pal" is a React-based web app that enables users to upload images and create color palettes by breaking down the image into its constituent pixels and projecting them into RGB color space. The app employs a K-means clustering algorithm to cluster similar pixels and generate a specified number of colors to represent the image. Users can select the number of colors they want in their palette and copy the corresponding hex codes for use in their design projects. The app runs entirely client-side, including the clustering algorithm, to ensure maximum performance. "Palette Pal" is an excellent tool for developers and designers who want to create color themes quickly based on images.`,
 	video: sampleVid,
 	githubLink: "https://github.com/MadisonEvans94/",
 	siteLink: "https://palettepal.herokuapp.com/",
@@ -27,9 +26,9 @@ const project1Content = {
 // BlueCloud
 const project2Content = {
 	title: "Blue Cloud Music",
-	description:
-		"Palette pal is a really cool app. I'm pretty proud of it, therefore, I'm taking this time to make a dummy description for a modal I'm making. In the real thing, this text will actually be meaningful, but rn, I'm just using it to fill out some space and allow me to see how things will eventually be layed out... so... yeah... Here I am. just filling out space. you know me. the usual. lol. asdf. asdf asdf asdf  fjasld;gk; asd;gjkas llaskjdhg laskdh lkajshdglakjshd lgjkashd lgkjhasldkgjhalsdk ga  alskjdgh laksjdgh lkajshdg jkashd glkajsh dglkjahsd lgjkl  s",
+	description: `"Blue Cloud Music" is a personal music website that I created using React and Django. As an artist, producer, and audio engineer with over a decade of experience, I am incredibly proud of this project, which showcases my music and creative abilities. The site features captivating animations, UI, and other design principles that I poured my heart into during the development process. Through this site, I am able to connect with my fans and share my music with the world. In addition, I plan to sell sample packs and plugins that I'm currently working on. "Blue Cloud Music" is a creative venture that allows me to express myself musically and share my passion with others.`,
 	video: sampleVid,
+	// [ ] update this github link
 	githubLink: "https://github.com/MadisonEvans94/",
 	siteLink: "https://palettepal.herokuapp.com/",
 };
@@ -37,9 +36,9 @@ const project2Content = {
 const project3Content = {
 	title: "Drop the Ball",
 	description:
-		"Palette pal is a really cool app. I'm pretty proud of it, therefore, I'm taking this time to make a dummy description for a modal I'm making. In the real thing, this text will actually be meaningful, but rn, I'm just using it to fill out some space and allow me to see how things will eventually be layed out... so... yeah... Here I am. just filling out space. you know me. the usual. lol. asdf. asdf asdf asdf  fjasld;gk; asd;gjkas llaskjdhg laskdh lkajshdglakjshd lgjkashd lgkjhasldkgjhalsdk ga  alskjdgh laksjdgh lkajshdg jkashd glkajsh dglkjahsd lgjkl  s",
+		"'Drop The Ball'is an interactive game developed using Vanilla Javascript and HTML Canvas, featuring animation and physics simulation. The game involves dropping a ball into a virtual peg board where the ball adheres to gravity and bounces off the pegs. The physics simulation is implemented using mathematical equations to accurately simulate the ball's movement and collision with the pegs. The HTML canvas is used to render the game graphics and create the user interface. Overall, 'Drop The Ball' is a fun and engaging project that showcases the power of Vanilla Javascript and HTML Canvas for game development.",
 	video: sampleVid,
-	githubLink: "https://github.com/MadisonEvans94/",
+	githubLink: "https://github.com/MadisonEvans94/phase-1-Project-1",
 	siteLink: "https://palettepal.herokuapp.com/",
 };
 function App() {
