6b932bc6cb72fa5ec96b06b2737c9bdf243e389a
Madison Evans <m.corbinevans@gmail.com>
2023-03-13 14:48:08 -0700

    frontend for email form created

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,66 +8,87 @@ const ContactPage = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		// Code to send email or submit contact form goes here
-		setSent(true);
+		const data = {
+			name: name,
+			email: email,
+			message: message,
+		};
+		fetch("/send-email", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				setSent(true);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
 	};
 
 	return (
-		<div style={{ minHeight: "100vh" }}>
-			{/* <h1>Contact Me</h1>
-			<form onSubmit={handleSubmit}>
-				<label>
-					Name:
-					<input
-						type="text"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-					/>
-				</label>
-				<label>
-					Email:
-					<input
-						type="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</label>
-				<label>
-					Message:
-					<textarea
-						value={message}
-						onChange={(e) => setMessage(e.target.value)}
-					/>
-				</label>
-				<button type="submit">Send</button>
+		<div
+			style={{ minHeight: "100vh" }}
+			className="
+			p-6 flex flex-col justify-center items-center">
+			<form
+				onSubmit={handleSubmit}
+				className="
+				flex flex-col w-full">
+				{/* name */}
+				<input
+					type="text"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					placeholder="Name"
+					className="my-2 p-2 rounded"
+				/>
+				{/* email */}
+				<input
+					type="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					placeholder="Email"
+					className="my-2 p-2 rounded"
+				/>
+				{/* message */}
+				<textarea
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+					placeholder="Message"
+					className="my-2 p-2 rounded"
+					rows="6"
+				/>
+				{/* send button */}
+				<button
+					type="submit"
+					className="my-2 p-2 rounded transition mx-24 border-white border text-white
+					hover:bg-blue-500 hover:border-blue-500">
+					Send
+				</button>
 			</form>
 			{sent && <p>Thank you for contacting me!</p>}
-			<p>Email me: your-email@example.com</p>
-			<p>Call me: 123-456-7890</p>
-			<p>Find me on social media:</p>
-			<ul>
-				<li>
-					<a href="https://www.linkedin.com/in/your-linkedin-profile">
-						LinkedIn
-					</a>
-				</li>
-				<li>
-					<a href="https://github.com/your-github-profile">GitHub</a>
-				</li>
-				<li>
-					<a href="https://twitter.com/your-twitter-profile">Twitter</a>
-				</li>
-			</ul>
-			<p>Visit me: 123 Main St, Anytown USA 12345</p>
-			<p>Business Hours: Monday - Friday 9am - 5pm EST</p>
-			<iframe
-				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.935478784341!2d-73.98785368459682!3d40.744734179329086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259f0b0e51e81%3A0xb1a0a87d84c0c191!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1587026266666!5m2!1sen!2sus"
-				width="600"
-				height="450"
-				frameborder="0"
-				allowfullscreen=""
-				aria-hidden="false"
-				tabindex="0"></iframe> */}
+			<div className="text-white w-full ">
+				<p>
+					Email me: <strong>M.Corbinevans@gmail.com</strong>{" "}
+				</p>
+				<p>Find me on social media:</p>
+				<ul>
+					<li>
+						<a href="https://www.linkedin.com/in/your-linkedin-profile">
+							LinkedIn
+						</a>
+					</li>
+					<li>
+						<a href="https://github.com/your-github-profile">GitHub</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
