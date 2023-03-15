3eb470c83c1eead8dddc025b236d9c8058785901
Madison Evans <m.corbinevans@gmail.com>
2023-03-14 20:55:54 -0700

    email functional

--- src/Components/Contact/Socials.jsx
+++ src/Components/Contact/Socials.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Socials = () => {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				"service_ptv88yz",
+				"template_tkul86p",
+				form.current,
+				"QXbk0V9wjQFcqNmT5"
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+	};
+
 	return (
-		<div
-			style={{ minHeight: "100vh" }}
-			className="
-			p-6 flex flex-col justify-center items-center">
-			<div>LinkedIn</div>
-			<div>Github</div>
-			<div>Email</div>
-		</div>
+		<form ref={form} onSubmit={sendEmail}>
+			<label>Name</label>
+			<input type="text" name="user_name" />
+			<label>Email</label>
+			<input type="email" name="user_email" />
+			<label>Message</label>
+			<textarea name="message" />
+			<input type="submit" value="Send" />
+		</form>
 	);
 };
 
