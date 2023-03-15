c36469b0ab7370faa945f0f8b67dfac410d10106
Madison Evans <m.corbinevans@gmail.com>
2023-03-15 13:32:52 -0700

    overscroll fix

--- src/Components/LayoutGrid/LayoutGrid.jsx
+++ src/Components/LayoutGrid/LayoutGrid.jsx
@@ -4,7 +4,7 @@ function LayoutGrid({ nav, content }) {
 	return (
 		<div className="fixed flex flex-col md:flex-row h-screen w-screen overflow-hidden overscroll-none">
 			<div className="w-full md:w-1/5 md:h-full">{nav}</div>
-			<div className="w-full md:w-4/5 bg-gray-600 md:h-full overflow-y-scroll">
+			<div className="w-full md:w-4/5 bg-gray-600 md:h-full overflow-y-scroll overflow-x-hidden overscroll-x-hidden">
 				{content}
 			</div>
 		</div>
