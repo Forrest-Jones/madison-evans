09a65524f823fb44f95db98eb950bfb85be8d9e6
Madison Evans <m.corbinevans@gmail.com>
2023-03-14 21:14:29 -0700

    form text clears on submit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,12 @@
 import React, { useRef } from "react";
+
 // TODO: DONT DELETE => import emailjs from "emailjs-com";
 
 const Contact = () => {
 	const form = useRef();
+	const nameInput = useRef();
+	const emailInput = useRef();
+	const messageInput = useRef();
 
 	const sendEmail = (e) => {
 		e.preventDefault();
@@ -23,34 +27,52 @@ const Contact = () => {
 		// 			console.log(error.text);
 		// 		}
 		// 	);
+
 		console.log("test mode");
+
+		// clear input values
+		nameInput.current.value = "";
+		emailInput.current.value = "";
+		messageInput.current.value = "";
 	};
 
 	return (
 		<div
 			style={{ minHeight: "100vh" }}
-			className="
-            p-6 flex flex-col justify-center items-center">
-			<div
-				className="
-                border p-12 h-fit rounded">
+			className="p-6 flex flex-col justify-center items-center">
+			<div className="border p-12 h-fit rounded">
 				<form
 					ref={form}
 					onSubmit={sendEmail}
-					className="
-                    flex flex-col justify-center items-center">
+					className="flex flex-col justify-center items-center">
 					<label className="w-full py-1 text-white">Name</label>
-					<input type="text" name="user_name" className="rounded" required />
+					<input
+						type="text"
+						name="user_name"
+						className="rounded p-1"
+						required
+						ref={nameInput}
+					/>
 					<label className="w-full py-1 text-white">Email</label>
-					<input type="email" name="user_email" className="rounded" required />
+					<input
+						type="email"
+						name="user_email"
+						className="rounded p-1"
+						required
+						ref={emailInput}
+					/>
 					<label className="w-full py-1 text-white">Message</label>
-					<textarea name="message" rows="6" className="rounded" required />
+					<textarea
+						name="message"
+						rows="6"
+						className="rounded p-1"
+						required
+						ref={messageInput}
+					/>
 					<input
 						type="submit"
 						value="Send"
-						className="
-                        text-white border rounded p-1 w-full my-3 cursor-pointer
-                        hover:bg-white hover:text-black transition"
+						className="text-white border rounded p-1 w-full my-3 cursor-pointer hover:bg-white hover:text-black transition"
 					/>
 				</form>
 			</div>
