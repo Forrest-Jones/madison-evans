93ce7aac975c1c0dc45391506dc4535476e36b9f
Madison Evans <m.corbinevans@gmail.com>
2023-03-14 18:15:46 -0700

    socials section added for testing

---
+++ src/Components/Contact/Socials.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Socials = () => {
+	return (
+		<div
+			style={{ minHeight: "100vh" }}
+			className="
+			p-6 flex flex-col justify-center items-center">
+			<div>LinkedIn</div>
+			<div>Github</div>
+			<div>Email</div>
+		</div>
+	);
+};
+
+export default Socials;
