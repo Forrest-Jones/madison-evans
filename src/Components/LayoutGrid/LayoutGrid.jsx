ef8e874b5297a39949c6a11d1fda0c5810e48dcc
Madison Evans <m.corbinevans@gmail.com>
2023-03-15 13:30:33 -0700

    fixing overscrolling issues

--- src/Components/LayoutGrid/LayoutGrid.jsx
+++ src/Components/LayoutGrid/LayoutGrid.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function LayoutGrid({ nav, content }) {
 	return (
-		<div className="fixed flex flex-col md:flex-row h-screen w-screen">
+		<div className="fixed flex flex-col md:flex-row h-screen w-screen overflow-hidden overscroll-none">
 			<div className="w-full md:w-1/5 md:h-full">{nav}</div>
 			<div className="w-full md:w-4/5 bg-gray-600 md:h-full overflow-y-scroll">
 				{content}
