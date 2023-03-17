db56937e221bcd7ab9aa3cd005af6b2ca06460bc
Madison Evans <m.corbinevans@gmail.com>
2023-03-17 16:32:48 -0700

    removing custom hook

--- src/App.js
+++ src/App.js
@@ -6,54 +6,12 @@ import LayoutGrid from "./Components/LayoutGrid/LayoutGrid";
 import Contact from "./Components/Contact/Contact";
 import ProjectPage from "./Pages/ProjectPage";
 import Skills from "./Components/Skills/Skills";
-import useScrollDirection from "./helpers/useScrollDirection";
 
 import Modal from "./Components/Modal/Modal";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-	const scrollDirection = useScrollDirection();
-
-	function enterFullscreen() {
-		const element = document.documentElement;
-
-		if (element.requestFullscreen) {
-			element.requestFullscreen();
-		} else if (element.mozRequestFullScreen) {
-			// Firefox
-			element.mozRequestFullScreen();
-		} else if (element.webkitRequestFullscreen) {
-			// Chrome, Safari, and Opera
-			element.webkitRequestFullscreen();
-		} else if (element.msRequestFullscreen) {
-			// IE/Edge
-			element.msRequestFullscreen();
-		}
-	}
-
-	function exitFullscreen() {
-		if (document.exitFullscreen) {
-			document.exitFullscreen();
-		} else if (document.mozCancelFullScreen) {
-			// Firefox
-			document.mozCancelFullScreen();
-		} else if (document.webkitExitFullscreen) {
-			// Chrome, Safari, and Opera
-			document.webkitExitFullscreen();
-		} else if (document.msExitFullscreen) {
-			// IE/Edge
-			document.msExitFullscreen();
-		}
-	}
-
-	useEffect(() => {
-		if (scrollDirection === "down") {
-			enterFullscreen();
-		} else if (scrollDirection === "up") {
-			exitFullscreen();
-		}
-	}, [scrollDirection]);
 	const [showModal, setShowModal] = useState(false);
 	const [modalContent, setModalContent] = useState(null);
 	return (
