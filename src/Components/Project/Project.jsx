8a0d068cf3fd51c4cb435351504a738b9692f35b
Madison Evans <m.corbinevans@gmail.com>
2023-03-17 14:25:10 -0700

    framer motion updates

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -15,9 +15,16 @@ function Project({ setShowModal, showModal, projectContent, image }) {
 	return (
 		<>
 			<motion.div
-				initial={{ scale: 1 }}
-				whileHover={{ scale: 1.05, y: -10 }}
-				transition={{ type: "spring", duration: 0.5 }}
+				initial={{ scale: 1, opacity: 0, y: 50 }}
+				whileHover={{
+					scale: 1.04,
+					y: -5,
+					opacity: 1,
+					transition: { type: "spring", bounce: 0.4 },
+				}}
+				whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+				viewport={{ amount: 1, once: true }}
+				transition={{ type: "spring", duration: 1 }}
 				className=" relative
 				my-4 w-32 h-32 mx-4 bg-center bg-cover cursor-pointer rounded-lg
 				md:h-32 md:w-32
--- src/Components/ProjectGroupContainer/ProjectGroupContainer.jsx
+++ src/Components/ProjectGroupContainer/ProjectGroupContainer.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 const ProjectGroupContainer = ({ children }) => {
 	return (
 		<div className="h-full">
-			<div
+			<motion.div
 				className=" h-full
 				w-full flex flex-col px-auto justify-start items-center pt-12
 				lg:justify-center lg:pt-0 lg:pb-12
 				lg:flex-row ">
 				{children}
-			</div>
+			</motion.div>
 		</div>
 	);
 };
