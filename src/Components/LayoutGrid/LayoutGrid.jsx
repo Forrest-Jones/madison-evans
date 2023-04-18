b9f91ebb6cc277bab9a7530717e49ed60f24ee6f
Madison Evans <m.corbinevans@gmail.com>
2023-04-18 14:40:06 -0700

    code cleanup

--- src/Components/LayoutGrid/LayoutGrid.jsx
+++ src/Components/LayoutGrid/LayoutGrid.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 function LayoutGrid({ children }) {
+	const navStyling =
+		"md:w-40 fixed flex flex-col justify-center h-full border border-red-500";
+	const contentStyling = "md:pl-40";
+	const containerStyling =
+		"fixed h-screen w-screen overflow-hidden overscroll-none md:h-full overflow-y-scroll overflow-x-hidden overscroll-x-hidden flex flex-row";
 	return (
-		<div className="fixed flex flex-col md:flex-row h-screen w-screen overflow-hidden overscroll-none">
-			{/* <div className="w-full md:w-1/5 md:h-full">{nav}</div> */}
-			<div className="w-full bg-gray-600 md:h-full overflow-y-scroll overflow-x-hidden overscroll-x-hidden">
-				{children}
-			</div>
+		<div className={containerStyling}>
+			<div className={navStyling}>{children[0]}</div>
+			<div className={contentStyling}>{children[1]}</div>
 		</div>
 	);
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ import { motion } from "framer-motion";
 const Navbar = () => {
 	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // state to track if mobile menu is open
 
-	// Container that holds the links in the navbar
 	function NavLinkContainer() {
 		const handleClickScroll = (event) => {
 			event.preventDefault();
@@ -18,59 +17,62 @@ const Navbar = () => {
 		};
 
 		return (
-			<motion.div className="flex-grow md:flex md:flex-col md:overflow-y-auto">
-				<motion.a
-					initial={{ x: 0 }}
-					whileHover={{ x: 5, transition: { type: "spring" } }}
-					href="#about-me"
-					className="text-secondary hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-					onClick={handleClickScroll}>
-					About Me
-				</motion.a>
-				<motion.a
-					initial={{ x: 0 }}
-					whileHover={{ x: 5, transition: { type: "spring" } }}
-					href="#skills"
-					className="text-secondary hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-					onClick={handleClickScroll}>
-					Skills
-				</motion.a>
-				<motion.a
-					initial={{ x: 0 }}
-					whileHover={{ x: 5, transition: { type: "spring" } }}
-					href="#experience"
-					className="text-secondary hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-					onClick={handleClickScroll}>
-					Experience
-				</motion.a>
-				<motion.a
-					initial={{ x: 0 }}
-					whileHover={{ x: 5, transition: { type: "spring" } }}
-					href="#projects"
-					className="text-secondary hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-					onClick={handleClickScroll}>
-					Projects
-				</motion.a>
-				<motion.a
-					initial={{ x: 0 }}
-					whileHover={{ x: 5, transition: { type: "spring" } }}
-					href="#contact"
-					className="text-secondary hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-					onClick={handleClickScroll}>
-					Contact
-				</motion.a>
-			</motion.div>
+			<div
+				className={`${
+					isMobileMenuOpen ? "block" : "hidden"
+				} md:flex-grow md:flex md:flex-col md:justify-start md:items-stretch md:min-h-screen`}>
+				<motion.div className="flex-grow md:flex md:flex-col md:overflow-y-auto">
+					<motion.a
+						initial={{ x: 0 }}
+						whileHover={{ x: 5, transition: { type: "spring" } }}
+						href="#about-me"
+						className="text-primary hover:text-primary block px-3 py-2 rounded-md text-base font-medium"
+						onClick={handleClickScroll}>
+						About Me
+					</motion.a>
+					<motion.a
+						initial={{ x: 0 }}
+						whileHover={{ x: 5, transition: { type: "spring" } }}
+						href="#skills"
+						className="text-primary hover:text-primary block px-3 py-2 rounded-md text-base font-medium"
+						onClick={handleClickScroll}>
+						Skills
+					</motion.a>
+					<motion.a
+						initial={{ x: 0 }}
+						whileHover={{ x: 5, transition: { type: "spring" } }}
+						href="#experience"
+						className="text-primary hover:text-primary block px-3 py-2 rounded-md text-base font-medium"
+						onClick={handleClickScroll}>
+						Experience
+					</motion.a>
+					<motion.a
+						initial={{ x: 0 }}
+						whileHover={{ x: 5, transition: { type: "spring" } }}
+						href="#projects"
+						className="text-primary hover:text-primary block px-3 py-2 rounded-md text-base font-medium"
+						onClick={handleClickScroll}>
+						Projects
+					</motion.a>
+					<motion.a
+						initial={{ x: 0 }}
+						whileHover={{ x: 5, transition: { type: "spring" } }}
+						href="#contact"
+						className="text-primary hover:text-primary block px-3 py-2 rounded-md text-base font-medium"
+						onClick={handleClickScroll}>
+						Contact
+					</motion.a>
+				</motion.div>
+			</div>
 		);
 	}
 
-	// Hamburger button inside the navbar
 	function HamburgerButton() {
 		return (
 			<button
 				onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
 				type="button"
-				className="inline-flex items-center justify-center p-2 rounded-md text-secondary hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white">
-				{/* Hamburger icon */}
+				className="inline-flex items-center justify-center p-2 rounded-md text-secondary hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white md:hidden">
 				<svg
 					className={`${isMobileMenuOpen ? "hidden" : "block"} h-6 w-6`}
 					xmlns="http://www.w3.org/2000/svg"
@@ -85,7 +87,6 @@ const Navbar = () => {
 						d="M4 6h16M4 12h16M4 18h16"
 					/>
 				</svg>
-				{/* Close icon */}
 				<svg
 					className={`${isMobileMenuOpen ? "block" : "hidden"} h-6 w-6`}
 					xmlns="http://www.w3.org/2000/svg"
@@ -105,15 +106,9 @@ const Navbar = () => {
 	}
 
 	return (
-		<nav className="bg-primary flex-shrink-0 p-4 absolute w-full md:w-40 xl:48">
-			<div className="md:hidden">{HamburgerButton()}</div>
-
-			<div
-				className={`${
-					isMobileMenuOpen ? "block" : "hidden"
-				} md:flex-grow md:flex md:flex-col md:justify-start md:items-stretch md:min-h-screen`}>
-				{NavLinkContainer()}
-			</div>
+		<nav className="flex flex-shrink-0 p-4 w-full">
+			<HamburgerButton />
+			<NavLinkContainer />
 		</nav>
 	);
 };
