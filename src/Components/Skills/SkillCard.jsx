8ca127e81bcfa41ae33978b0fb277f583342e1c7
Madison Evans <m.corbinevans@gmail.com>
2023-04-24 12:13:41 -0700

    styling

--- src/Components/Skills/SkillCard.jsx
+++ src/Components/Skills/SkillCard.jsx
@@ -15,40 +15,22 @@ const SkillCard = (props) => {
 			layout
 			initial={{ scale: 1 }}
 			onClick={toggleBoxShadow}
-			className={`${
-				boxShadowActive
-					? "shadow-[20px_20px_0_0_rgba(0,0,0,.3)] absolute w-2/3 h-2/3 top-[20%] bg-light"
-					: "shadow-[0px_0px_0_0_rgba(0,0,0,.3)] md:w-48 lg:w-60 xl:80"
-			}
-          cursor-pointer border hover:bg-light bg-primary border-secondary text-info mt-8 py-8 rounded-xl flex flex-col items-center justify-start required:h-1/3 md:mx-4 md:my-0 hover:shadow-[20px_20px_0_0_rgba(0,0,0,.3)]
-        `}>
+			className="border border-secondary rounded-lg mx-4 p-2 w-60 flex flex-col items-center hover:shadow-[20px_20px_0_0_rgba(0,0,0,0.3)] transition">
 			<motion.div
-				transition={{ type: "spring", damping: 20, stiffness: 80 }}
-				className="
-          flex flex-row items-center justify-center w-full 
-          md:flex-col">
-				<motion.div>
+				layout
+				transition={{ type: "spring", damping: 20, stiffness: 80 }}>
+				<motion.div className="flex flex-col items-center" layout>
 					<Frontend className="text-accent w-24 h-24 lg:w-36 lg:h-36" />
-					<motion.h2 className="text-center text-lg lg:text-lg font-semibold text-info font-display">
-						{title}
-					</motion.h2>
+					<h2 className="font-bold font-display text-lg">{title}</h2>
 				</motion.div>
 
 				<ul
 					initial={{ opacity: 0, y: 30 }}
-					whileInView={{
-						opacity: 1,
-						y: 0,
-						transition: { delay: 0.5, duration: 1 },
-					}}
-					viewport={{ amount: 1 }}
 					transition={{ type: "spring" }}
-					className="w-fit px-6 flex flex-col overflow-hidden my-3">
+					className="">
 					{content.map((content, id) => {
 						return (
-							<motion.li
-								key={id}
-								className="text-center text-xs lg:text-sm text-secondary ">
+							<motion.li layout key={id} className="">
 								{content}
 							</motion.li>
 						);
