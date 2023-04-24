b9af17e7d3bb83dc5928f28f3bbb2e2205107f5c
Madison Evans <m.corbinevans@gmail.com>
2023-04-24 12:31:21 -0700

    skill section refactoring

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,21 +3,13 @@ import SkillCard from "./SkillCard";
 import jsIcon from "../../assets/svg/icons8-javascript-logo.svg";
 import backendIcon from "../../assets/svg/backend.svg";
 import uxuiIcon from "../../assets/svg/design.svg";
+import skillCardInfo from "./SkillCardInfo.json";
 
+console.log(skillCardInfo);
 const Modal = () => {
 	return <></>;
 };
 const Skills = () => {
-	const frontendTitle = "Frontend";
-	const backendTitle = "Backend";
-	const uxuiTitle = "UX/UI";
-	const frontendContent = ["HTML, CSS, JavaScript", "React", "UX/UI"];
-	const backendContent = [
-		"Java, Node, Python",
-		"Django",
-		"Flask",
-		"Database Management",
-	];
 	const uxuiContent = ["HCI core concepts", "Figma", "Procreate, Blender"];
 	return (
 		<div
@@ -26,17 +18,14 @@ const Skills = () => {
 			className="relative flex h-fit w-full flex-col items-center justify-around bg-primary">
 			<div className="flex h-fit w-full flex-col justify-center md:flex-row">
 				{/* TODO: Refactor with json and have the skill cards mapped over */}
-				<SkillCard
-					title={frontendTitle}
-					content={frontendContent}
-					icon={jsIcon}
-				/>
-				<SkillCard
-					title={backendTitle}
-					content={backendContent}
-					icon={backendIcon}
-				/>
-				<SkillCard title={uxuiTitle} content={uxuiContent} icon={uxuiIcon} />
+				{skillCardInfo.map((item) => {
+					return (
+						<SkillCard title="title" content={["content"]} icon={jsIcon} />
+					);
+				})}
+				{/* <SkillCard title="title" content={["content"]} icon={jsIcon} />
+				<SkillCard title="title" content={["content"]} icon={jsIcon} />
+				<SkillCard title="title" content={["content"]} icon={jsIcon} /> */}
 				<Modal />
 			</div>
 		</div>
