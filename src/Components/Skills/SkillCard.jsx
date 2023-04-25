0e192e03386a32d4178a15338b228886a000189f
Madison Evans <m.corbinevans@gmail.com>
2023-04-24 18:38:10 -0700

    styling

--- src/Components/Skills/SkillCard.jsx
+++ src/Components/Skills/SkillCard.jsx
@@ -1,27 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { BsWindowFullscreen as Frontend } from "react-icons/bs";
-
+import { useState } from "react";
+import colors from "../../helpers/colors";
 const SkillCard = (props) => {
-	const { title, content, setModal } = props;
+	const [layout, setLayout] = useState(0);
+	const { title, content } = props;
+	const closedLayout =
+		"mx-4 flex w-60 cursor-pointer flex-col items-center rounded-lg border border-secondary p-2 hover:bg-light hover:shadow-[20px_20px_0_0_rgba(0,0,0,0.3)]";
+	const openedLayout =
+		"absolute cursor-pointer rounded-lg border border-secondary w-full";
+	function toggleLayout() {
+		layout === 0 ? setLayout(1) : setLayout(0);
+	}
+	const dynamicStyle =
+		layout === 0
+			? {
+					marginInline: "1rem",
+					display: "flex",
+					width: "15rem",
+					flexDirection: "column",
+					alignItems: "center",
+					borderRadius: "0.5rem",
+					border: `1px solid ${colors.secondary}`,
+					padding: "0.5rem",
+					cursor: "pointer",
+			  }
+			: {
+					marginInline: "1rem",
+					display: "flex",
+					width: "30em",
+					height: "30em",
+					flexDirection: "column",
+					alignItems: "center",
+					borderRadius: "0.5rem",
+					border: `1px solid ${colors.secondary}`,
+					padding: "0.5rem",
+					cursor: "pointer",
+			  };
 	return (
 		<motion.div
+			layout
 			onClick={() => {
-				setModal({
-					title: title,
-					content: content,
-				});
+				toggleLayout();
 			}}
-			layoutId="skill-container"
-			className="mx-4 flex w-60 cursor-pointer flex-col items-center rounded-lg border border-secondary p-2 transition hover:bg-light hover:shadow-[20px_20px_0_0_rgba(0,0,0,0.3)]">
+			style={dynamicStyle}>
 			<div>
 				<div className="flex flex-col items-center">
-					<motion.div layoutId="skill-icon">
+					<motion.div layout>
 						<Frontend className="h-24 w-24 text-accent lg:h-36 lg:w-36" />
 					</motion.div>
-					<motion.h2
-						layoutId="skill-title"
-						className="font-display text-lg font-bold">
+					<motion.h2 layout className="font-display text-lg font-bold">
 						{title}
 					</motion.h2>
 				</div>
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -2,61 +2,24 @@ import React from "react";
 import SkillCard from "./SkillCard";
 import jsIcon from "../../assets/svg/icons8-javascript-logo.svg";
 import skillCardInfo from "./SkillCardInfo.json";
-import { useState } from "react";
-import { BsWindowFullscreen as Frontend } from "react-icons/bs";
-import { motion } from "framer-motion";
 
-const Modal = ({ title, content, setModal }) => {
-	return (
-		<>
-			<motion.div
-				layoutId="skill-container"
-				className="absolute top-0 mt-8 h-[90%] w-5/6 cursor-pointer rounded-2xl border border-secondary bg-primary shadow-[20px_20px_0_0_rgba(0,0,0,0.3)]"
-				onClick={() => {
-					setModal(null);
-				}}>
-				<div className="flex flex-row px-4">
-					<motion.div layoutId="skill-icon">
-						<Frontend className="h-24 w-24 text-accent lg:h-36 lg:w-36" />
-					</motion.div>
-					<motion.h1
-						layoutId="skill-title"
-						className="flex w-full flex-row items-center px-8 text-center font-display text-4xl font-bold">
-						{title}
-					</motion.h1>
-				</div>
-				{content.map((item) => (
-					<motion.div>{item}</motion.div>
-				))}
-			</motion.div>
-		</>
-	);
-};
 const Skills = () => {
-	const [modal, setModal] = useState(null);
 	return (
 		<div
 			id="skills"
-			className="relative flex h-screen w-full flex-col items-center justify-around bg-primary">
+			className="flex h-screen w-full flex-col items-center justify-around bg-primary">
 			<div className="flex h-fit w-full flex-col justify-center md:flex-row">
-				{skillCardInfo.map((skill) => {
+				{skillCardInfo.map((skill, key) => {
 					return (
 						<SkillCard
+							key={key}
 							title={skill.title}
 							content={skill.subSkills}
 							icon={jsIcon}
-							setModal={setModal}
 						/>
 					);
 				})}
 			</div>
-			{modal && (
-				<Modal
-					title={modal.title}
-					content={modal.content}
-					setModal={setModal}
-				/>
-			)}
 		</div>
 	);
 };
