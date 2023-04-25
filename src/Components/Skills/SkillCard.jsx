52f922d114593665f64fee134a253962a42c2b73
Madison Evans <m.corbinevans@gmail.com>
2023-04-24 20:17:31 -0700

    styling

--- src/Components/Skills/SkillCard.jsx
+++ src/Components/Skills/SkillCard.jsx
@@ -1,24 +1,33 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { BsWindowFullscreen as Frontend } from "react-icons/bs";
-import { useState } from "react";
 import colors from "../../helpers/colors";
+
+const containerVariants = {
+	closed: {},
+	opened: {},
+};
+
+const ulVariants = {
+	hidden: { opacity: 0 },
+	visible: { opacity: 1, transition: { delay: 0.3 } }, // Add a delay to control the stagger effect
+	exited: { opacity: 0, transition: { delay: 0.3 } },
+};
+
 const SkillCard = (props) => {
 	const [layout, setLayout] = useState(0);
 	const { title, content } = props;
-	const closedLayout =
-		"mx-4 flex w-60 cursor-pointer flex-col items-center rounded-lg border border-secondary p-2 hover:bg-light hover:shadow-[20px_20px_0_0_rgba(0,0,0,0.3)]";
-	const openedLayout =
-		"absolute cursor-pointer rounded-lg border border-secondary w-full";
+
 	function toggleLayout() {
 		layout === 0 ? setLayout(1) : setLayout(0);
 	}
-	const dynamicStyle =
+
+	const dynamicContainerStyle =
 		layout === 0
 			? {
 					marginInline: "1rem",
 					display: "flex",
-					width: "15rem",
+					width: "15em",
 					flexDirection: "column",
 					alignItems: "center",
 					borderRadius: "0.5rem",
@@ -29,8 +38,8 @@ const SkillCard = (props) => {
 			: {
 					marginInline: "1rem",
 					display: "flex",
-					width: "30em",
-					height: "30em",
+					width: "15em",
+					height: "fit",
 					flexDirection: "column",
 					alignItems: "center",
 					borderRadius: "0.5rem",
@@ -38,30 +47,53 @@ const SkillCard = (props) => {
 					padding: "0.5rem",
 					cursor: "pointer",
 			  };
-	return (
-		<motion.div
-			layout
-			onClick={() => {
-				toggleLayout();
-			}}
-			style={dynamicStyle}>
-			<div>
-				<div className="flex flex-col items-center">
-					<motion.div layout>
-						<Frontend className="h-24 w-24 text-accent lg:h-36 lg:w-36" />
-					</motion.div>
-					<motion.h2 layout className="font-display text-lg font-bold">
-						{title}
-					</motion.h2>
-				</div>
 
-				<ul>
-					{content.map((content, id) => {
-						return <li key={id}>{content}</li>;
-					})}
-				</ul>
-			</div>
-		</motion.div>
+	const dynamicHeaderStyle =
+		layout === 0
+			? { display: "flex", flexDirection: "column", alignItems: "center" }
+			: {
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					width: "100%",
+			  };
+
+	return (
+		<AnimatePresence mode="wait">
+			<motion.div
+				layout
+				onClick={() => {
+					toggleLayout();
+				}}
+				style={dynamicContainerStyle}
+				variants={containerVariants} // Add variants to the container
+				animate={layout === 0 ? "closed" : "opened"}>
+				<div className="w-full">
+					<motion.div layout style={dynamicHeaderStyle}>
+						<motion.div layout>
+							<Frontend className="h-24 w-24 text-accent lg:h-36 lg:w-36" />
+						</motion.div>
+						<motion.h2 layout className="font-display">
+							{title}
+						</motion.h2>
+					</motion.div>
+
+					{layout === 1 && (
+						<motion.ul
+							layout
+							className="text-center"
+							variants={ulVariants} // Add variants to the motion.ul element
+							initial="hidden"
+							animate="visible"
+							exit="exited">
+							{content.map((content, id) => {
+								return <li key={id}>{content}</li>;
+							})}
+						</motion.ul>
+					)}
+				</div>
+			</motion.div>
+		</AnimatePresence>
 	);
 };
 
